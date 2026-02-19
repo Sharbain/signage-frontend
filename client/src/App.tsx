febc7ce8ffe623b0aac01ec872b5ae219e2e7313
Sharbain <yazan@plutoad.com>
2026-02-19 18:15:00 +0300

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import { CommandStatusBar } from "./components/CommandStatusBar";
@@ -17,35 +17,220 @@ import ClientDetailPage from "./pages/ClientDetailPage";
 import GroupDetailPage from "./pages/GroupDetailPage";
 import DevicesLayout from "./layouts/DevicesLayout";
 
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const location = useLocation();
+  const token = localStorage.getItem("accessToken");
+
+  if (!token) {
+    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
+  }
+
+  return children;
+}
+
+function Shell({ children }: { children: JSX.Element }) {
+  return (
+    <div className="flex min-h-screen bg-[#f5f5f0]">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <div className="flex-1 overflow-y-auto">{children}</div>
+      </div>
+      <CommandStatusBar />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <div className="flex min-h-screen bg-[#f5f5f0]">
-        <Sidebar />
-        <div className="flex-1 flex flex-col">
-          <Navbar />
-          <div className="flex-1 overflow-y-auto">
-            <Routes>
-              <Route path="/" element={<div className="p-6"><Dashboard /></div>} />
-              <Route path="/devices" element={<DevicesWorkspace />} />
-              <Route path="/devices/add" element={<div className="p-6"><AddDevice /></div>} />
-              <Route path="/devices/:id" element={<DevicesLayout><DeviceControlPage /></DevicesLayout>} />
-              <Route path="/content" element={<div className="p-6"><ContentScheduler /></div>} />
-              <Route path="/template" element={<div className="p-6"><TemplateDesigner /></div>} />
-              <Route path="/schedule" element={<div className="p-6"><SchedulePage /></div>} />
-              <Route path="/monitor" element={<div className="p-6"><MonitorPage /></div>} />
-              <Route path="/clients" element={<div className="p-6"><ClientsPage /></div>} />
-              <Route path="/clients/:id" element={<div className="p-6"><ClientDetailPage /></div>} />
-              <Route path="/groups" element={<Navigate to="/devices" replace />} />
-              <Route path="/groups/new" element={<div className="p-6"><GroupDetailPage /></div>} />
-              <Route path="/groups/:groupId" element={<div className="p-6"><GroupDetailPage /></div>} />
-              <Route path="/device-map" element={<Navigate to="/devices" replace />} />
-              <Route path="/login" element={<div className="p-6"><Login /></div>} />
-            </Routes>
-          </div>
-        </div>
-        <CommandStatusBar />
-      </div>
+      <Routes>
+        {/* Public */}
+        <Route
+          path="/login"
+          element={
+            <div className="min-h-screen bg-[#f5f5f0] p-6">
+              <Login />
+            </div>
+          }
+        />
+
+        {/* Protected */}
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <Dashboard />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/devices"
+          element={
+            <RequireAuth>
+              <Shell>
+                <DevicesWorkspace />
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/devices/add"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <AddDevice />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/devices/:id"
+          element={
+            <RequireAuth>
+              <Shell>
+                <DevicesLayout>
+                  <DeviceControlPage />
+                </DevicesLayout>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/content"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <ContentScheduler />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/template"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <TemplateDesigner />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/schedule"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <SchedulePage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/monitor"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <MonitorPage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/clients"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <ClientsPage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/clients/:id"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <ClientDetailPage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/groups"
+          element={
+            <RequireAuth>
+              <Navigate to="/devices" replace />
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/groups/new"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <GroupDetailPage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/groups/:groupId"
+          element={
+            <RequireAuth>
+              <Shell>
+                <div className="p-6">
+                  <GroupDetailPage />
+                </div>
+              </Shell>
+            </RequireAuth>
+          }
+        />
+
+        <Route
+          path="/device-map"
+          element={
+            <RequireAuth>
+              <Navigate to="/devices" replace />
+            </RequireAuth>
+          }
+        />
+
+        {/* catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </Router>
   );
 }
